2aeaa669d957ba6fcec8d78fc30d57b4fcd9c204
mdroybal <danroybal@yahoo.com>
2017-02-13 14:45:07 -0700

    Second Commit

---
+++ documentation/readme.txt
@@ -0,0 +1,4 @@
+Readme
+
+The collections_demo.py is a simple program to show how the collection counter method counts the elements in a string. The collections module is a standard module in Python 2.7.13.
+
---
+++ source_code/collections_demo.py
@@ -0,0 +1,11 @@
+# Roybal, Monte 2-13-2017
+# Homework 2
+# I am implementing a simple source code utilizing the collections module.
+
+import collections
+
+new = collections.Counter("This is a simple string")
+new['z'] = 0
+print new
+print list(new.elements())
+
